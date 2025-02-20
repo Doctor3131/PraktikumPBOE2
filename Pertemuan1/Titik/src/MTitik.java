a42ef796d2cfc69e77ee66b0660686f649815eba
Doctor3131 <sirielwnf@gmail.com>
2025-02-20 11:42:13 +0700

    Titik, Praktikum PBO Pertemuan 1 selesai

--- Pertemuan1/Titik/src/MTitik.java
+++ Pertemuan1/Titik/src/MTitik.java
@@ -1,3 +1,10 @@
+/*
+* Nama File    : MTitik.java
+* Deskripsi    : Berisi atribut dan method dalam class Titik
+* Pembuat      : Siriel Wafa Nuriel Fahri
+* Tanggal      : 20 Februari 2025
+*/
+
 public class MTitik {
     public static void main(String[] args) {
         Titik T1 = new Titik(4,3);
@@ -10,6 +17,7 @@ public class MTitik {
         T1.printTitik();
         System.out.println("");
 
+
         Titik T2 = T1;
         System.out.print("Titik T2: ");
         T2.printTitik();
@@ -19,16 +27,88 @@ public class MTitik {
         T2.printTitik();
         System.out.println("");
 
+
         Titik T3 = new Titik();
-        Titik T4 = new Titik(3,5);
+        // Titik T4 = new Titik(3,5);
         System.out.print("Titik T3: ");
         T3.printTitik();
-        System.out.print("Titik T4: ");
-        T4.printTitik();
+        // System.out.print("Titik T4: ");
+        // T4.printTitik();
         System.out.println("Jumlah Objek Titik = " + Titik.getCounterTitik());
         // System.out.println("Jumlah obyek Titik = " + T2.getCounterTitik());
         System.out.println("");
 
+
+        // main-main kuadran
+        Titik Kuadran1 = new Titik(3, 4);
+        Titik Kuadran2 = new Titik(-3, 4);
+        Titik Kuadran3 = new Titik(-3, -4);
+        Titik Kuadran4 = new Titik(3, -4);
+
+        System.out.println("Kuadran titik (" + Kuadran1.getAbsis() + "," + Kuadran1.getOrdinat() + ") = " + Kuadran1.getKuadran());
+        System.out.println("Kuadran titik (" + Kuadran2.getAbsis() + "," + Kuadran2.getOrdinat() + ") = " + Kuadran2.getKuadran());
+        System.out.println("Kuadran titik (" + Kuadran3.getAbsis() + "," + Kuadran3.getOrdinat() + ") = " + Kuadran3.getKuadran());
+        System.out.println("Kuadran titik (" + Kuadran4.getAbsis() + "," + Kuadran4.getOrdinat() + ") = " + Kuadran4.getKuadran());
+        System.out.println("");
+
+
+        // main-main jarak dari titik pusat
+        System.out.println("Jarak dari titik pusat dengan titik (" + Kuadran1.getAbsis() + "," + Kuadran1.getOrdinat() + ") = " + Kuadran1.getJarakPusat());
+        System.out.println("Jarak dari titik pusat dengan titik (" + Kuadran2.getAbsis() + "," + Kuadran2.getOrdinat() + ") = " + Kuadran2.getJarakPusat());
+        System.out.println("Jarak dari titik pusat dengan titik (" + Kuadran3.getAbsis() + "," + Kuadran3.getOrdinat() + ") = " + Kuadran3.getJarakPusat());
+        System.out.println("Jarak dari titik pusat dengan titik (" + Kuadran4.getAbsis() + "," + Kuadran4.getOrdinat() + ") = " + Kuadran4.getJarakPusat());
+        System.out.println("");
+
+
+        // main-main jarak dari titik pusat
+        Titik T = new Titik(3,4);
+        System.out.println("Titik T = (" + T.getAbsis() + "," + T.getOrdinat() + ")");
+        System.out.println("Jarak dari titik T dengan titik (" + Kuadran1.getAbsis() + "," + Kuadran1.getOrdinat() + ") = " + Kuadran1.getJarak(T));
+        System.out.println("Jarak dari titik T dengan titik (" + Kuadran2.getAbsis() + "," + Kuadran2.getOrdinat() + ") = " + Kuadran2.getJarak(T));
+        System.out.println("Jarak dari titik T dengan titik (" + Kuadran3.getAbsis() + "," + Kuadran3.getOrdinat() + ") = " + Kuadran3.getJarak(T));
+        System.out.println("Jarak dari titik T dengan titik (" + Kuadran4.getAbsis() + "," + Kuadran4.getOrdinat() + ") = " + Kuadran4.getJarak(T));
+        System.out.println("");
+     
         
+        // main-main refleksi part 1
+        Kuadran1.refleksiX();
+        Kuadran1.refleksiY();
+        System.out.println("");
+        Kuadran2.refleksiX();
+        Kuadran2.refleksiY();
+        System.out.println("");
+        Kuadran3.refleksiX();
+        Kuadran3.refleksiY();
+        System.out.println("");
+        Kuadran4.refleksiX();
+        Kuadran4.refleksiY();
+        System.out.println("");
+
+
+        // main-main refleksi part 2
+        System.out.print("refleksi sumbu x dengan fungsi get, titik: (" + Kuadran1.getAbsis() + "," + Kuadran1.getOrdinat() + ") = " );
+        Kuadran1.getRefleksiX().printTitik();
+        System.out.print("refleksi sumbu y dengan fungsi get, titik: (" + Kuadran1.getAbsis() + "," + Kuadran1.getOrdinat() + ") = " );
+        Kuadran1.getRefleksiY().printTitik();
+        System.out.println("");
+        
+        System.out.print("refleksi sumbu x dengan fungsi get, titik: (" + Kuadran2.getAbsis() + "," + Kuadran2.getOrdinat() + ") = " );
+        Kuadran2.getRefleksiX().printTitik();
+        System.out.print("refleksi sumbu y dengan fungsi get, titik: (" + Kuadran2.getAbsis() + "," + Kuadran2.getOrdinat() + ") = " );
+        Kuadran2.getRefleksiY().printTitik();
+        System.out.println("");
+        
+        System.out.print("refleksi sumbu x  dengan fungsi get, titik: (" + Kuadran3.getAbsis() + "," + Kuadran3.getOrdinat() + ") = " );
+        Kuadran3.getRefleksiX().printTitik();
+        System.out.print("refleksi sumbu y  dengan fungsi get, titik: (" + Kuadran3.getAbsis() + "," + Kuadran3.getOrdinat() + ") = " );
+        Kuadran3.getRefleksiY().printTitik();
+        System.out.println("");
+
+        System.out.print("refleksi sumbu x  dengan fungsi get, titik: (" + Kuadran4.getAbsis() + "," + Kuadran4.getOrdinat() + ") = " );
+        Kuadran4.getRefleksiX().printTitik();
+        System.out.print("refleksi sumbu y  dengan fungsi get, titik: (" + Kuadran4.getAbsis() + "," + Kuadran4.getOrdinat() + ") = " );
+        Kuadran4.getRefleksiY().printTitik();
+        System.out.println("");
+
     }
 }
--- Pertemuan1/Titik/src/Titik.java
+++ Pertemuan1/Titik/src/Titik.java
@@ -1,46 +1,110 @@
+/*
+ * Nama File    : Titik.java
+ * Deskripsi    : Berisi atribut dan method dalam class Titik
+ * Pembuat      : Siriel Wafa Nuriel Fahri
+ * Tanggal      : 20 Februari 2025
+ */
+
 public class Titik {
+    /*******************ATRIBUT**********************/
     double absis;
     double ordinat;
     static int counterTitik = 0;
 
+    /*******************METHOD***********************/
+    // konstruktor untuk membuat titik (0,0) dan menyimpan jumlah object titik di class Titik
     Titik() {
         absis = 0;
         ordinat = 0;
         counterTitik++;
     }
-
+    
+    // konstruktor untuk membuat titik (x,y) dan menyimpan jumlah object titik di class Titik
     Titik(double x, double y) {
         absis = x;
         ordinat = y;
         counterTitik++;
     } 
 
+    // mengembalikan nilai absis 
     double getAbsis() {
         return absis;
     }
 
+    // mengembalikan nilai ordinat
     double getOrdinat() {
         return ordinat;
     }
 
+    // merubah nilai absis dengan x
     void setAbsis(double x) {
         absis = x;
     }
 
+    // merubah nilai ordinat dengan y
     void setOrdinat(double y) {
         ordinat = y;
     }
 
+    // menggeser titik 
     void geser(double x, double y) {
         absis = absis + x;
         ordinat = ordinat + y;
     }
 
+    // menampilkan nilai titik
     void printTitik() {
         System.out.println("Titik ( " + absis + " , " + ordinat + " )");
     }
 
+    // mengembalikan nilai CounterTitik yaitu n banyak titik yang dibuat
     static int getCounterTitik() {
         return counterTitik;
     }
+
+    // mengembalikan nilai kuadran titik tersebut
+    int getKuadran() {
+        if (absis > 0 && ordinat > 0) {
+            return 1;
+        } else if (absis < 0 && ordinat > 0) {
+            return 2;
+        } else if (absis < 0 && ordinat < 0) {
+            return 3;
+        } else if (absis > 0 && ordinat < 0) {
+            return 4;
+        } else { 
+            return -1; // jika tidak masuk ke kuadran
+        }
+    }
+
+    // mengembalikan nilai jarak titik dari titik pusat (0,0)
+    double getJarakPusat() {
+        return Math.sqrt(Math.pow(absis, 2) + Math.pow(ordinat, 2));
+    } 
+    
+    // mengembalikan nilai jarak titik dengan titik T
+    double getJarak(Titik T) {
+        return Math.sqrt(Math.pow(this.absis - T.absis, 2) + Math.pow(this.ordinat - T.ordinat, 2));
+    }
+
+    // membuat refleksi sumbu x dari titik
+    void refleksiX() {
+        System.out.println("Refleksi sumbu X titik (" + this.getAbsis() + "," + this.getOrdinat() + ") adalah: " + this.getAbsis() + "," + this.getOrdinat()*-1);
+    }
+
+    // membuat refleksi sumbu y dari titik
+    void refleksiY() {
+        System.out.println("Refleksi sumbu Y titik (" + this.getAbsis() + "," + this.getOrdinat() + ") adalah: " + this.getAbsis()*-1 + "," + this.getOrdinat());
+    }
+
+    // mengembalikan nilai refksi terhadap sumbu X
+    Titik getRefleksiX() {
+        return new Titik(absis, ordinat*-1);
+    }
+
+    // mengembalikan nilai refleksi terhadap sumbu Y
+    Titik getRefleksiY() {
+        return new Titik(absis*-1, ordinat);
+    }
 }
+
