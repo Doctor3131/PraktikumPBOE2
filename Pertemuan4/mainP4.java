c48a127bf3af8fee185cd858db277876d61277bd
Doctor3131 <sirielwnf@gmail.com>
2025-03-13 11:09:18 +0700

    just a daily commit

--- Pertemuan4/mainP4.java
+++ Pertemuan4/mainP4.java
@@ -1,5 +1,20 @@
 public class mainP4 {
     public static void main(String[] args) {
-        
+        // Membuat objek bangun datar
+        BangunDatar bd = new BangunDatar();
+        bd.setJmlSisi(3);
+        bd.setWarna("merah");
+        bd.setBorder("hitam");
+
+        // Membuat objek persegi
+        Persegi p = new Persegi(6, "Hijau", "kuning");
+
+        System.out.println("");
+        System.out.println("Info Bangun Datar");
+        bd.printInfo();
+
+        System.out.println("");
+        System.out.println("Info Persegi");
+        p.printInfo();
     }
 }
