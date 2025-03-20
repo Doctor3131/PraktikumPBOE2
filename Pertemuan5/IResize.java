99fa82866134a8e998e9e756bf6025d1c6de7ead
Doctor3131 <sirielwnf@gmail.com>
2025-03-20 11:09:15 +0700

    praktikum 20 Maret 2025

---
+++ Pertemuan5/IResize.java
@@ -0,0 +1,14 @@
+/*
+* Nama File    : IResize
+* Pembuat      : Siriel Wafa Nuriel Fahri
+* Tanggal      : 20 Maret 2025
+*/
+
+
+public interface IResize {
+    public void zoomIn();
+
+    public void zoomOut();
+
+    public void zoom(int percent);
+}
