b8682af252c2a1d4c0bd2f4cd4908897b769fa77
Doctor3131 <sirielwnf@gmail.com>
2025-03-20 11:08:50 +0700

    comment code nya

--- Pertemuan4/Persegi.java
+++ Pertemuan4/Persegi.java
@@ -4,57 +4,57 @@
 * Tanggal      : 13 Maret 2025
 */
 
-public class Persegi extends BangunDatar {
-    private double sisi;
+// public class Persegi extends BangunDatar {
+//     private double sisi;
 
-    public Persegi() {
-        setJmlSisi(4);
-    }
+//     public Persegi() {
+//         setJmlSisi(4);
+//     }
 
-    // public Persegi(double sisi, String warna, String border) {
-    //     this.sisi = sisi;
-    //     setWarna(warna);
-    //     setBorder(border);
-    //     setJmlSisi(4);
-    // }
+//     // public Persegi(double sisi, String warna, String border) {
+//     //     this.sisi = sisi;
+//     //     setWarna(warna);
+//     //     setBorder(border);
+//     //     setJmlSisi(4);
+//     // }
 
-    public Persegi(double sisi, String warna, String border) {
-        super(4, warna,border);
-        this.sisi = sisi;
-    }
+//     public Persegi(double sisi, String warna, String border) {
+//         super(4, warna,border);
+//         this.sisi = sisi;
+//     }
 
-    public double getSisi() {
-        return sisi;
-    }
+//     public double getSisi() {
+//         return sisi;
+//     }
 
-    public void setSisi(double sisi) {
-        this.sisi = sisi;
-    }
+//     public void setSisi(double sisi) {
+//         this.sisi = sisi;
+//     }
 
-    public double getLuas() {
-        return 4 * sisi;
-    }
+//     public double getLuas() {
+//         return 4 * sisi;
+//     }
 
-    public double getDiagonal() {
-        return sisi * Math.sqrt(2);
-    }
+//     public double getDiagonal() {
+//         return sisi * Math.sqrt(2);
+//     }
 
-    @Override
-    public void printInfo() {
-        System.out.println("Jumlah sisi: " + getJmlSisi());
-        System.out.println("Warna: " + getWarna());
-        System.out.println("Border: " + getBorder());
-        System.out.println("Sisi: " + sisi);
-    }
+//     @Override
+//     public void printInfo() {
+//         System.out.println("Jumlah sisi: " + getJmlSisi());
+//         System.out.println("Warna: " + getWarna());
+//         System.out.println("Border: " + getBorder());
+//         System.out.println("Sisi: " + sisi);
+//     }
 
-    // @Override
-    // public void printInfo() {
-    //     super.printInfo();
-    //     System.out.println("Sisi: " + sisi);
-    // }
+//     // @Override
+//     // public void printInfo() {
+//     //     super.printInfo();
+//     //     System.out.println("Sisi: " + sisi);
+//     // }
 
-    // @Override
-    // public static void printCounterBangunDatar() {
-    //     System.out.println("");
-    // }
-}
+//     // @Override
+//     // public static void printCounterBangunDatar() {
+//     //     System.out.println("");
+//     // }
+// }
