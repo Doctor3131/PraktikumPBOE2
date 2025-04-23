b802b34ddfdda9d2e74d9a7e27d499a7d134c8db
Doctor3131 <sirielwnf@gmail.com>
2025-04-24 00:03:34 +0700

    p

---
+++ Pertemuan6/PostTest/Programmer.java
@@ -0,0 +1,10 @@
+package Pertemuan6.PostTest;
+
+public class Programmer extends Pegawai {
+    private int bonus = 450000;
+
+    public Programmer(String nama) {
+        setNama(nama);
+        
+    }
+}
---
+++ Pertemuan6/PostTest/TestPolimorfisme.java
@@ -0,0 +1,8 @@
+package Pertemuan6.PostTest;
+
+public class TestPolimorfisme {
+    public static void main(String[] args) {
+
+        Pegawai pegawai = new Programmer("Mira");
+    }
+}
