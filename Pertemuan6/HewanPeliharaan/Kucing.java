cf8b89f54f66b8b652bf006147ab290a06bd6fd5
Doctor3131 <sirielwnf@gmail.com>
2025-04-27 13:22:04 +0700

    tugas praktikum selesai

---
+++ Pertemuan6/HewanPeliharaan/Kucing.java
@@ -0,0 +1,26 @@
+/*
+* Nama File    : Kucing.java
+* Deskripsi    : class kucing child dari anabul
+* Pembuat      : Siriel Wafa Nuriel Fahri
+* Tanggal      : 25/04/25
+*/
+
+
+package Pertemuan6.HewanPeliharaan;
+
+public class Kucing extends Anabul {
+    // Constructor
+    public Kucing(String nama) {
+        super(nama);
+    }
+    
+    @Override
+    public void gerak() {
+        System.out.println("Kucing bergerak dengan melata");
+    }
+    
+    @Override
+    public void bersuara() {
+        System.out.println("Kucing berbunyi meong");
+    }
+}
--- Pertemuan6/PostTest/TestPolimorfisme.java
+++ Pertemuan6/PostTest/TestPolimorfisme.java
@@ -14,14 +14,23 @@ public class TestPolimorfisme {
         Pegawai pegawai = new Programmer("Mira");
         Pegawai pegawai2 = new Manager("Joko");
         Manager pegawai3 = new Manager("Argo");
+        Programmer pegawai4 = new Programmer("Arem");
+        Programmer pegawai5 = new Programmer("Ma");
 
         ArrayList<Pegawai> emps = new ArrayList<>();
         emps.add(pegawai);
         emps.add(pegawai2);
         emps.add(pegawai3);
+        emps.add(pegawai4);
+        emps.add(pegawai5);
 
         for (Pegawai emp : emps) {
             emp.tampilData();
         }
     }
 }
+
+/*
+ * Penjelasan Manfaat Polimorfisme
+ * Kita dapat secara langsung mengelompokkan pelbagai pegawai ke dalam ArrayList
+ */
