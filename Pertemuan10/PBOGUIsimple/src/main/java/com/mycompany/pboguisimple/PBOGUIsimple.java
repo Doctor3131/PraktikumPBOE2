2eaca0d941f5fe7a8756887c414cb7be51991b0b
Doctor3131 <sirielwnf@gmail.com>
2025-05-30 00:51:20 +0700

    lupa commit

---
+++ Pertemuan10/PBOGUIsimple/src/main/java/com/mycompany/pboguisimple/PBOGUIsimple.java
@@ -0,0 +1,12 @@
+package com.mycompany.pboguisimple;
+
+/**
+ *
+ * @author sirie
+ */
+public class PBOGUIsimple {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
